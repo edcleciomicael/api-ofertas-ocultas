0aa68d565b9e878324039375153a625b4eecb2c8
edcleciomicael <edcleciomicael@gmail.com>
2026-05-06 15:30:06 -0300

    ajuste

--- api/meliService.js
+++ api/meliService.js
@@ -2,10 +2,14 @@ const axios = require("axios");
 
 async function searchProducts(query) {
   try {
-    const url = `https://api.mercadolibre.com/sites/MLB/search?q=${encodeURIComponent(query)}`;
-
     const response = await axios.get(
-      `https://api.allorigins.win/raw?url=${encodeURIComponent(url)}`
+      "https://api.mercadolibre.com/sites/MLB/search",
+      {
+        params: {
+          q: query,
+          limit: 20
+        }
+      }
     );
 
     const data = response.data;
@@ -27,51 +31,6 @@ async function searchProducts(query) {
     }));
 
   } catch (error) {
-    console.error("ERRO PROXY:", error.message);
-    return [];
-  }
-}
-
-module.exports = { searchProducts };
-
-/*const axios = require("axios");
-
-async function searchProducts(query) {
-  try {
-    const response = await axios.get(
-      "https://api.mercadolibre.com/sites/MLB/search",
-      {
-        params: {
-          q: query,
-          limit: 20
-        },
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
-          "Accept": "application/json",
-          "Accept-Language": "pt-BR",
-          "Connection": "keep-alive"
-        }
-      }
-    );
-
-    return response.data.results.map(item => ({
-      external_id: item.id,
-      name: item.title,
-      price: item.price,
-      old_price: item.original_price || null,
-      discount: item.original_price
-        ? Math.round(
-            ((item.original_price - item.price) / item.original_price) * 100
-          )
-        : null, // 🔥 mudou aqui
-      image: item.thumbnail,
-      link: item.permalink,
-      sales: item.sold_quantity,
-      free_shipping: item.shipping?.free_shipping
-    }));
-
-  } catch (error) {
     console.error(
       "Erro Mercado Livre:",
       error.response?.status,
@@ -81,4 +40,4 @@ async function searchProducts(query) {
   }
 }
 
-module.exports = { searchProducts }; */
+module.exports = { searchProducts };
